5f48e956f708c70c151334707a81f70282e10bad
Nayoon Kim <nayoon030303@gmail.com>
2021-04-15 23:10:22 +0900

    img search

--- winformCrawling/Form1.Designer.cs
+++ winformCrawling/Form1.Designer.cs
@@ -29,13 +29,250 @@ namespace winformCrawling
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tboxID = new System.Windows.Forms.TextBox();
+            this.tboxPW = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tboxUrl = new System.Windows.Forms.TextBox();
+            this.tboxSearch = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pboxMain = new System.Windows.Forms.PictureBox();
+            this.btnPre = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.tboxGo = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tboxID
+            // 
+            this.tboxID.Location = new System.Drawing.Point(77, 43);
+            this.tboxID.Name = "tboxID";
+            this.tboxID.Size = new System.Drawing.Size(125, 27);
+            this.tboxID.TabIndex = 1;
+            this.tboxID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tboxPW
+            // 
+            this.tboxPW.Location = new System.Drawing.Point(77, 76);
+            this.tboxPW.Name = "tboxPW";
+            this.tboxPW.Size = new System.Drawing.Size(125, 27);
+            this.tboxPW.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.tboxID);
+            this.groupBox1.Controls.Add(this.tboxPW);
+            this.groupBox1.Location = new System.Drawing.Point(30, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(354, 120);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1.특정 Site Login";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(25, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PW:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(34, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Controls.Add(this.tboxUrl);
+            this.groupBox2.Controls.Add(this.tboxSearch);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(30, 177);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(354, 261);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "2.1.이미지 검색";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(249, 29);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(77, 32);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "조회";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tboxUrl
+            // 
+            this.tboxUrl.Location = new System.Drawing.Point(77, 78);
+            this.tboxUrl.Name = "tboxUrl";
+            this.tboxUrl.Size = new System.Drawing.Size(249, 27);
+            this.tboxUrl.TabIndex = 6;
+            this.tboxUrl.TextChanged += new System.EventHandler(this.tboxUrl_TextChanged);
+            // 
+            // tboxSearch
+            // 
+            this.tboxSearch.Location = new System.Drawing.Point(77, 34);
+            this.tboxSearch.Name = "tboxSearch";
+            this.tboxSearch.Size = new System.Drawing.Size(148, 27);
+            this.tboxSearch.TabIndex = 5;
+            this.tboxSearch.TextChanged += new System.EventHandler(this.tboxSearch_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(6, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "검색어:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pboxMain);
+            this.groupBox3.Location = new System.Drawing.Point(390, 21);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(398, 367);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "2.2.이미지 검색 상새";
+            // 
+            // pboxMain
+            // 
+            this.pboxMain.Location = new System.Drawing.Point(6, 26);
+            this.pboxMain.Name = "pboxMain";
+            this.pboxMain.Size = new System.Drawing.Size(386, 335);
+            this.pboxMain.TabIndex = 0;
+            this.pboxMain.TabStop = false;
+            // 
+            // btnPre
+            // 
+            this.btnPre.Location = new System.Drawing.Point(428, 394);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(63, 29);
+            this.btnPre.TabIndex = 6;
+            this.btnPre.Text = "<";
+            this.btnPre.UseVisualStyleBackColor = true;
+            this.btnPre.Click += new System.EventHandler(this.btnPre_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(687, 394);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(63, 29);
+            this.btnNext.TabIndex = 7;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // tboxGo
+            // 
+            this.tboxGo.Location = new System.Drawing.Point(518, 396);
+            this.tboxGo.Name = "tboxGo";
+            this.tboxGo.Size = new System.Drawing.Size(43, 27);
+            this.tboxGo.TabIndex = 8;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("맑은 고딕", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTotal.Location = new System.Drawing.Point(567, 398);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(35, 25);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "/ 0";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(608, 394);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(64, 29);
+            this.btnGo.TabIndex = 10;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.tboxGo);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPre);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pboxMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tboxID;
+        private System.Windows.Forms.TextBox tboxPW;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tboxUrl;
+        private System.Windows.Forms.TextBox tboxSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pboxMain;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox tboxGo;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnGo;
     }
 }
 
--- winformCrawling/Form1.cs
+++ winformCrawling/Form1.cs
@@ -1,16 +1,13 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Text;
+using System.IO;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+
 
 namespace winformCrawling
 {
@@ -28,7 +25,161 @@ namespace winformCrawling
 
             _options = new ChromeOptions();
             _options.AddArgument("disable-gpu");
+
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = tboxID.Text;
+            string pw = tboxPW.Text;
+
+            _driver = new ChromeDriver(_driverService, _options);
+            _driver.Navigate().GoToUrl("https://www.naver.com/"); //웹사이트에 접속
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            var element = _driver.FindElementByXPath("//*[@id='account']/a"); //Main 로그인 버트
+
+            element.Click();
+
+            Thread.Sleep(3000);
+
+            element = _driver.FindElementByXPath("//*[@id='id']"); //ID 입력창 
+            element.SendKeys(id);
+
+            element = _driver.FindElementByXPath("//*[@id='pw']"); //PW 입력창
+            element.SendKeys(pw);
+           
+            element = _driver.FindElementByXPath("//*[@id='log.login']");//로그인 버튼
+            element.Click();
+           
+        }
+
+        List<string> Lsrc = new List<string>(); // IMG URL
+        int i = 0; //현재 배열 위치
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string strURL = "https://www.google.com/search?q=" + tboxSearch.Text + "&source=lnms&tbm=isch";
+
+            _driver = new ChromeDriver(_driverService, _options);
+            _driver.Navigate().GoToUrl(strURL);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            _driver.ExecuteScript("window.scrollBy(0, 10000)");
+
+   
+
+            Lsrc = new List<string>();
+            foreach (IWebElement item in _driver.FindElementsByClassName("rg_i"))
+            {
+                if (item.GetAttribute("src") != null)
+                    Lsrc.Add(item.GetAttribute("src"));
+            }
+
+
+            lblTotal.Text = "/ " + Lsrc.Count.ToString();
+
+            this.Invoke(new Action(delegate ()
+            {
+                try
+                {
+                    foreach (string strsrc in Lsrc)
+                    {
+                        i++;
+
+                        GetMapImage(Lsrc[i]);
+                        tboxGo.Text = i.ToString();
+                        Refresh();
+                        Thread.Sleep(50);
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }));
+        }
+
+        private void GetMapImage(string base64String)
+        {
+            try
+            {
+                var base64Data = Regex.Match(base64String, @"data:image/(?<type>.+?),(?<data>.+)").Groups["data"].Value;  // 정규식 검색
+                var binData = Convert.FromBase64String(base64Data);
+
+                using (var stream = new MemoryStream(binData))
+                {
+                    if (stream.Length == 0)
+                    {
+                        pboxMain.Load(base64String);
+                        tboxGo.Text = i.ToString();
+                        tboxUrl.Text = base64String;
+                    }
+                    else
+                    {
+                        var image = Image.FromStream(stream);
+                        pboxMain.Image = image;
+                        tboxUrl.Text = base64String;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tboxUrl_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tboxSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPre_Click(object sender, EventArgs e)
+        {
+            this.Invoke(new Action(delegate ()
+            {
+                i--;
+
+                GetMapImage(Lsrc[i]);
+                tboxGo.Text = i.ToString();
+            }));
+
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            this.Invoke(new Action(delegate ()
+            {
+                i++;
+
+                GetMapImage(Lsrc[i]);
+                tboxGo.Text = i.ToString();
+            }));
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            this.Invoke(new Action(delegate ()
+            {
+                i = int.Parse(tboxGo.Text);
+
+                GetMapImage(Lsrc[i]);
+                tboxGo.Text = i.ToString();
+            }));
+        }
     }
 }
