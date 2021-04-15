05091a789df57413165b0d8431fa70c0f57c27c3
Nayoon Kim <nayoon030303@gmail.com>
2021-04-15 09:35:33 +0900

    초기 커밋.

---
+++ winformCrawling/Form1.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winformCrawling
+{
+    public partial class Form1 : Form
+    {
+        private ChromeDriverService _driverService = null;
+        private ChromeOptions _options = null;
+        private ChromeDriver _driver = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
+
+            _options = new ChromeOptions();
+            _options.AddArgument("disable-gpu");
+        }
+
+    }
+}
